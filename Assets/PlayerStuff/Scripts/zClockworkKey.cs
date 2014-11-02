02dab2dd57bbbe26a6fd914288fd98241aac03e0
Christian Cheung <cheung.christian@gmail.com>
2014-11-02 20:10:37 +0100

    Zombie: added clockwork-key onto the back
    The clockwork-key is parented to the chest-bone on the zombie in Unity
    (NOT Blender). A custom script gets the zombie-animator-state and turns
    the key accordingly

---
+++ Assets/PlayerStuff/Scripts/zClockworkKey.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zClockworkKey : MonoBehaviour {
+
+	Animator anim;
+
+
+	// The process can probably be more efficient using hashes instead!
+	// So look at this at later stage - when optimising
+	//////////////////////////////////////////////////////////////////
+//	int Run_hash = Animator.StringToHash("Run");
+//	int Jump_hash = Animator.StringToHash("Jump");
+//	int DblJump_hash = Animator.StringToHash("DoubleJump");
+//	int KnckBcn_hash = Animator.StringToHash("KnockBackb");
+//	int AtWeak_hash = Animator.StringToHash("Weak");
+//	int AtWeak2_hash = Animator.StringToHash("Weak2");
+//	int AtWSlide_hash = Animator.StringToHash("WeakSlidingAttack");
+//	int AtWAOE_hash = Animator.StringToHash("WeakAOE");
+//	int AtSmash_hash = Animator.StringToHash("SmashFwd");
+//	int AtSAOE_hash = Animator.StringToHash("StrongAOE");
+
+	// Use this for initialization
+	void Start () {
+
+		if (transform.parent != null && transform.parent.parent.parent.parent.parent.parent.parent.tag == "Player"){
+			Debug.Log("zKey found its parent!");
+			anim = transform.parent.parent.parent.parent.parent.parent.parent.GetComponent<Animator>();	
+		}
+		else{
+			Debug.LogError("zKey failed to find parent");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+		if (stateInfo.IsName("Run")||
+		    stateInfo.IsName("Jump")||
+		    stateInfo.IsName("DoubleJump")||
+		    stateInfo.IsName("KnockBackb")||
+		    stateInfo.IsName("Weak")||
+		    stateInfo.IsName("Weak2")||
+		    stateInfo.IsName("WeakSlidingAttack")||
+		    stateInfo.IsName("WeakAOE")){
+
+			transform.Rotate (0,0,Time.deltaTime*300);
+		}
+		else if (stateInfo.IsName("SmashFwd")||		    
+		         stateInfo.IsName("StrongAOE")){
+
+			transform.Rotate (0,0,Time.deltaTime*500);
+		}
+		else {
+		transform.Rotate (0,0,Time.deltaTime*100);
+		}
+
+
+	}
+}
